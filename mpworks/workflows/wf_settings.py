46f1d1b302f12947c658dd237aed3ea584c5f73d
specter119 <specter119@gmail.com>
2018-10-09 14:52:55 +0800

    change single cpu task to cpu

--- mpworks/workflows/wf_settings.py
+++ mpworks/workflows/wf_settings.py
@@ -9,10 +9,10 @@ __date__ = 'May 10, 2013'
 # nodes configuration will be ignored on SLURM due to different naming convention (nnodes vs nodes)
 QA_VASP = {'nnodes': 2, 'nodes': 2, 'walltime': '6:00:00'}
 QA_VASP_SMALL = {'nnodes': 2, 'nodes': 2, 'walltime': '6:00:00'}  # small walltime jobs
-# QA_DB = {'nnodes': 1, 'nodes': 1, 'ntasks_per_node': 1, 'walltime': '2:00:00'} # run db and boltztrap in 1 core
-QA_DB = {'nnodes': None, 'nodes': None, 'ntasks_per_node': None, 'queue': 'serial', 'qos': 'QosPartitionSerial', 'walltime': '2:00:00'}
-# QA_CONTROL = {'nnodes': 1, 'nodes': 1, 'ntasks_per_node': 1, 'walltime': '00:30:00'}
-QA_CONTROL = {'nnodes': None, 'nodes': None, 'ntasks_per_node': None, 'queue': 'serial', 'qos': 'QosPartitionSerial', 'walltime': '00:30:00'}
+QA_DB = {'nnodes': 1, 'nodes': 1, 'ntasks_per_node': 1, 'walltime': '2:00:00'} # run db and boltztrap in 1 core
+# QA_DB = {'nnodes': None, 'nodes': None, 'ntasks_per_node': None, 'queue': 'serial', 'qos': 'QosPartitionSerial', 'walltime': '2:00:00'}
+QA_CONTROL = {'nnodes': 1, 'nodes': 1, 'ntasks_per_node': 1, 'walltime': '00:30:00'}
+# QA_CONTROL = {'nnodes': None, 'nodes': None, 'ntasks_per_node': None, 'queue': 'serial', 'qos': 'QosPartitionSerial', 'walltime': '00:30:00'}
 
 MOVE_TO_GARDEN_DEV = False
 MOVE_TO_GARDEN_PROD = False
